6126d20481bfef84a83e5b7aa9fafcef01fe392a
Pedro Duran <pedro.duran@mobivery.com>
2013-06-18 16:39:56 +0200

    Added new project

---
+++ MalcomLibProject/MalcomLib-iphonesimulatorTests/MalcomLib_iphonesimulatorTests.m
@@ -0,0 +1,36 @@
+//
+//  MalcomLib_iphonesimulatorTests.m
+//  MalcomLib-iphonesimulatorTests
+//
+//  Created by Pedro Durán on 18/06/13.
+//  Copyright (c) 2013 Malcom. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MalcomLib_iphonesimulatorTests : XCTestCase
+
+@end
+
+@implementation MalcomLib_iphonesimulatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
---
+++ MalcomLibProject/MalcomLibProjectTests/MalcomLibProjectTests.m
@@ -0,0 +1,36 @@
+//
+//  MalcomLibProjectTests.m
+//  MalcomLibProjectTests
+//
+//  Created by Pedro Durán on 18/06/13.
+//  Copyright (c) 2013 Malcom. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MalcomLibProjectTests : XCTestCase
+
+@end
+
+@implementation MalcomLibProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
