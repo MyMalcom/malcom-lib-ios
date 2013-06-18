4899ffbf5ddce438286656ee314bcc5f779373c3
Pedro Duran <pedro.duran@mobivery.com>
2013-06-18 17:24:40 +0200

    Added targets for IOS lib

---
+++ MalcomLibProject/MalcomLib-iphoneos/MalcomLib_iphoneos.h
@@ -0,0 +1,13 @@
+//
+//  MalcomLib_iphoneos.h
+//  MalcomLib-iphoneos
+//
+//  Created by Pedro Durán on 18/06/13.
+//  Copyright (c) 2013 Malcom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MalcomLib_iphoneos : NSObject
+
+@end
---
+++ MalcomLibProject/MalcomLim-phonesimulator/MalcomLim_phonesimulator.h
@@ -0,0 +1,13 @@
+//
+//  MalcomLim_phonesimulator.h
+//  MalcomLim-phonesimulator
+//
+//  Created by Pedro Durán on 18/06/13.
+//  Copyright (c) 2013 Malcom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MalcomLim_phonesimulator : NSObject
+
+@end
