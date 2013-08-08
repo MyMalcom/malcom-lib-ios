979eaee66edc7e18f6245830eabadb93c310ea47
Pedro Duran <pedro.duran@mobivery.com>
2013-08-08 13:01:42 +0200

    New privateEndBeacon method to avoid duplicate beacons

--- Libraries/source/lib/MCMLib/src/MCMCore/MalcomLib.m
+++ Libraries/source/lib/MCMLib/src/MCMCore/MalcomLib.m
@@ -17,9 +17,13 @@
 #import "MCMCampaignsManager.h"
 #import "MCMStatsDefines.h"
 
+@interface MalcomLib ()
+
++ (void)privateEndBeacon;
+
+@end
 
 @implementation MalcomLib
-
 
 #pragma mark - Core methods
 
@@ -182,10 +186,10 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationWillTerminateNotification object:nil];
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationWillResignActiveNotification object:nil];
     
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(endBeacon)
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(privateEndBeacon)
                                                  name:UIApplicationDidEnterBackgroundNotification object:nil];
     
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(endBeacon)
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(privateEndBeacon)
                                                  name:UIApplicationWillTerminateNotification object:nil];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(resumeMalcom)
@@ -204,9 +208,11 @@
 }
 
 + (void)endBeacon {
-    
+    NSLog(@"[MalcomLib endBeacon] - This method is no more needed");
+}
+
++ (void)privateEndBeacon {
     [[MCMStatsManager sharedInstance] endBeacon];
-    
 }
 
 + (void)startBeaconWithName:(NSString *)name {
