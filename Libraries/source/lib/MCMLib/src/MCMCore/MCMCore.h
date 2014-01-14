4636a70656152f92ce0b54c66ff12b5aefba60ce
Guillermo Ceballos <willeeh@gmail.com>
2014-01-14 12:35:25 +0100

    Upgrated version to 2.0.9

--- Libraries/source/lib/MCMLib/src/MCMCore/MCMCore.h
+++ Libraries/source/lib/MCMLib/src/MCMCore/MCMCore.h
@@ -17,7 +17,7 @@
 #define MCMFailReceiveAd @"mcmFailReceiveAd"
 #define MCMNotificationAdsAreOff @"mcmNotificationAdsAreOff"
 
-#define MCMVersionSDK @"2.0.8"
+#define MCMVersionSDK @"2.0.9"
 
 
 //General imports
