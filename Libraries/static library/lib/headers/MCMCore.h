9d39d6645b475b6fd4460dbb0c64bbf6b1bfbf7b
Guillermo Ceballos <willeeh@gmail.com>
2014-02-26 13:13:17 +0100

    Prepared new minor version

--- Libraries/static library/lib/headers/MCMCore.h
+++ Libraries/static library/lib/headers/MCMCore.h
@@ -17,7 +17,7 @@
 #define MCMFailReceiveAd @"mcmFailReceiveAd"
 #define MCMNotificationAdsAreOff @"mcmNotificationAdsAreOff"
 
-#define MCMVersionSDK @"2.0.11"
+#define MCMVersionSDK @"2.0.12"
 
 
 //General imports
